6e3ad30f97fd78204afff4153c0db797c4b6eb9c
vincent4dust <vincent.dust4nothing@outlook.com>
2022-11-30 15:17:08 +0800

    [ Update data domain ad group ]
    - Update main.py
    - Update parameter_config.properties
    - Update README.md

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # sqlwarehouse_ddl_generator
+Assumption:
+1. The view schema name is concat from the database schema name
+2. Only string value in column db_name, tbl_name, col_name
+3. Make sure the source input file is csv format & no redundant row at the last row
+
 How to run the program?
 1. Fill in ../config/parameter_config.properties
 - SOURCE_FILE_NAME
-- MEMBER_AD_GROUP
+- ENV
 2. Fill in ../config/table_list.txt
 3. Put the source file (csv format) in ../source/
 4. Run ../app/main.py
--- app/main.py
+++ app/main.py
@@ -14,6 +14,12 @@ LOG_FILE_PATH = os.path.join(PROJECT_ROOT, "log", "log.txt")
 logging.basicConfig(filename=LOG_FILE_PATH, level=logging.INFO, format="%(asctime)s :: %(levelname)s :: %(message)s")
 
 def get_parameter_config(parameter_config_path=PARAMETER_CONFIG_PATH):
+    """
+    Read the parameter config file
+    :param src parameter_config_path: parameter file path
+    :return: config
+    :rtype config: dict
+    """
     config_parser = configparser.ConfigParser()
     config_parser.read(parameter_config_path)
     config = {}
@@ -30,6 +36,12 @@ def get_parameter_config(parameter_config_path=PARAMETER_CONFIG_PATH):
     return config
 
 def get_table_list(table_list_path=TABLE_LIST_PATH):
+    """
+    Get the table list to generate sql
+    :param table_list_path: table list file path
+    :return: table_list
+    :rtype table_list: list
+    """
     table_list = []
     if os.path.exists(table_list_path):
         with open(table_list_path, "r") as in_file:
@@ -42,6 +54,13 @@ def get_table_list(table_list_path=TABLE_LIST_PATH):
     return table_list
 
 def read_input_file(source_file, source_folder=SOURCE_FILE_FOLDER):
+    """
+    Read the input csv file
+    :param str source_file: file name with csv extension
+    :param str source_folder: input folder
+    :return: src_data
+    :rtype src_data: dataframe
+    """
     csv_source_file = os.path.join(source_folder, source_file)
     if os.path.exists(csv_source_file):
         if csv_source_file.endswith(".csv"):
@@ -53,7 +72,15 @@ def read_input_file(source_file, source_folder=SOURCE_FILE_FOLDER):
 
     return src_data
 
-def get_sql(src_data, table_list, ad_group):
+def get_sql(src_data, table_list, ad_group_list):
+    """
+    To generate the sql
+    :param str src_data: read in data
+    :param list table_list: table list to generate the sql
+    :param dict ad_group_list: the list of ad group of different domain
+    :return: sql_stmt
+    :rtype sql_stmt: str
+    """
     src_col = src_data.columns
     if "DB_NAME" in src_col:
         src_data["DB_NAME"] = src_data["DB_NAME"].apply(lambda x: x.upper())
@@ -79,23 +106,33 @@ def get_sql(src_data, table_list, ad_group):
             temp_df = src_data[src_data["TBL_NAME"] == table_name]
 
             db_name_list = temp_df["DB_NAME"].unique()
+            # make sure the same table name has same schema name
             if len(db_name_list) == 1:
                 db_schema_name = db_name_list[0]
             else:
                 logging.error("'DB_NAME' is not unique for table '{}'".format(table_name))
                 raise UserWarning("{} :: ERROR :: 'DB_NAME' is not unique for table '{}'".format(datetime.now(), table_name))
 
+            # view schema name concat from database schema name
             view_schema_name = db_schema_name[:-6]
 
             col_list = temp_df["COL_NAME"].tolist()
             col_sql_stmt = ""
             for col_name in col_list:
+                # determine the PII column name
                 conf_value = temp_df[temp_df["COL_NAME"] == col_name]["DATA CLASSIFICATION"].values.astype(str)[0]
                 if conf_value == "Highly Confidential":
+                    # determine the ad group of respective domain
+                    domain_value = temp_df[temp_df["COL_NAME"] == col_name]["DATA DOMAIN"].values.astype(str)[0]
+                    if domain_value.upper() in ad_group_list:
+                        ad_group = ad_group_list[domain_value.upper()]
+                    else:
+                        raise UserWarning("{} :: ERROR :: '{}' not found in data domain list".format(datetime.now(), domain_value))
                     col_sql_stmt += f"CASE\n\tWHEN IS_MEMBER('{ad_group}') THEN {col_name}\n\tELSE 'NO ACCESS'\nEND AS {col_name},\n"
                 else:
                     col_sql_stmt += f"{col_name},\n"
 
+            # get history
             his_table_name = table_name + "_HIS"
             col_his_sql_stmt = col_sql_stmt
             his_col_list = ["IS_ACTIVE", "EFF_ST_DT", "EFF_END_DT"]
@@ -136,6 +173,13 @@ def get_sql(src_data, table_list, ad_group):
     return sql_stmt
 
 def write_to_output(curr_time, sql_stmt, output_folder=OUTPUT_FILE_FOLDER):
+    """
+    Write a sql file
+    :param str curr_time: current time to name output file
+    :param str sql_stmt: content to write in the output file
+    :param str output_folder: the output folder
+    :return:
+    """
     file_name = f"{curr_time}.sql"
     output_file_name = os.path.join(output_folder, file_name)
     try:
@@ -146,17 +190,25 @@ def write_to_output(curr_time, sql_stmt, output_folder=OUTPUT_FILE_FOLDER):
         raise UserWarning("{} :: ERROR :: Failed to write to output folder".format(datetime.now()))
 
 def main():
+    """
+    Main function
+    :return:
+    """
     curr_time = datetime.now().strftime("%Y%m%d_%H%M%S")
     logging.info("########################### {}.sql ###########################".format(curr_time))
 
     config = get_parameter_config()
     table_list = get_table_list()
+
     source_file = config["DEFAULT"]["SOURCE_FILE_NAME"]
     logging.info("Source file name: {}".format(source_file))
-    ad_group = config["DEFAULT"]["MEMBER_AD_GROUP"]
-    logging.info("AD group: {}".format(ad_group))
+
+    env = config["DEFAULT"]["ENV"]
+    ad_group_list = config[env]
+    logging.info("Env: {}".format(env))
+
     src_data = read_input_file(source_file)
-    sql_stmt = get_sql(src_data, table_list, ad_group)
+    sql_stmt = get_sql(src_data, table_list, ad_group_list)
     write_to_output(curr_time, sql_stmt, output_folder=OUTPUT_FILE_FOLDER)
 
 if __name__ == "__main__":
